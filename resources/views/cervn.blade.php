66870baa15b63d21c8a861333e59f6ef2d6248f6
rm-miyatake <89385120+rm-miyatake@users.noreply.github.com>
2021-08-30 13:59:01 +0900

    cervnビューのプロトタイプ追加

--- resources/views/cervn.blade.php
+++ resources/views/cervn.blade.php
@@ -5,13 +5,52 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're in cervn!
-                </div>
-            </div>
-        </div>
+    <section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-20">
+      <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">cervn page</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">here is the cervn below</p>
     </div>
+    <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+      <table class="table-auto w-full text-left whitespace-no-wrap">
+        <thead>
+          <tr>
+            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">No.</th>
+            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">ID</th>
+            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">名前</th>
+            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">担当者名</th>
+            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">URL</th>
+            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">検出日</th>
+            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">確認日</th>
+            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">操作</th>
+            <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+          </tr>
+        </thead>
+        <tbody>
+          </tr>
+          <tr>
+            <td class="border-t-2 border-b-2 border-gray-200 px-4 py-3">Exclusive</td>
+            <td class="border-t-2 border-b-2 border-gray-200 px-4 py-3">48 Mb/s</td>
+            <td class="border-t-2 border-b-2 border-gray-200 px-4 py-3">120 GB</td>
+            <td class="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-lg text-gray-900">$72</td>
+            <td class="border-t-2 border-b-2 border-gray-200 px-4 py-3">Exclusive</td>
+            <td class="border-t-2 border-b-2 border-gray-200 px-4 py-3">48 Mb/s</td>
+            <td class="border-t-2 border-b-2 border-gray-200 px-4 py-3">120 GB</td>
+            <td class="border-t-2 border-b-2 border-gray-200 w-10 text-center">
+              <input name="plan" type="radio">
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="flex pl-4 mt-4 lg:w-2/3 w-full mx-auto">
+      <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+          <path d="M5 12h14M12 5l7 7-7 7"></path>
+        </svg>
+      </a>
+      <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+    </div>
+  </div>
+</section>
 </x-app-layout>
