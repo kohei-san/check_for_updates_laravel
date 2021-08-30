6f80bd063e23b33708bf2624f8537cf6542deb65
rm-miyatake <89385120+rm-miyatake@users.noreply.github.com>
2021-08-30 13:39:34 +0900

    login画面、rememberトークンを常時ONに変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,8 +35,7 @@
             <!-- Remember Me -->
             <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <input id="remember_me" type="hidden" value="on" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
                 </label>
             </div>
 
