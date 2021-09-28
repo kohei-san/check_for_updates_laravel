a43fe5fd8ce18ffa6d3df0920c65bdaf2cb3fa9b
rm-miyatake <89385120+rm-miyatake@users.noreply.github.com>
2021-09-28 15:50:53 +0900

    ローカルのPython起動機能のオミット

--- app/Http/Controllers/PythonController.php
+++ app/Http/Controllers/PythonController.php
@@ -11,26 +11,32 @@ class PythonController extends Controller
         // 本番環境のpythonディレクトリ
         $env = config('app.env');
         if($env == 'production'){
-            $dir = __DIR__;
-            $python_dir = '/home/xs330114/anaconda3/bin/python3.8';
-            $py_file = $python_dir.' '.$dir.'/test_server/server_test.py 2>&1';
+            // $dir = __DIR__;
+            // $python_dir = '/home/xs330114/anaconda3/bin/python3.8';
+            // $py_file = $python_dir.' '.$dir.'/test_server/server_test.py 2>&1';
 
-            exec($py_file, $output, $result);
+            // exec($py_file, $output, $result);
+
+            // return redirect()->intended(RouteServiceProvider::HOME)
+            // ->with(['message' => $output[0],
+            // 'status' => 'info']);
 
             return redirect()->intended(RouteServiceProvider::HOME)
-            ->with(['message' => $output[0],
-            'status' => 'info']);
+            ->with(['message' => 'この機能は現在準備中です。',
+                    'status' => 'info']);
         }
 
         // 開発環境のpythonディレクトリ
-        $dir = __DIR__;
-        $python_dir = 'C:\Users\miyatake\anaconda3\python.exe';
-        // $py_file = $python_dir.' '.$dir.'\test_server\server_test.py 2>&1';
-        $py_file = 'python'.' '.$dir.'\test_server\server_test.py 2>&1';
-        exec($py_file, $output, $result);
+        // $dir = __DIR__;
+        // $python_dir = 'C:\Users\miyatake\anaconda3\python.exe';
+        // $py_file = 'python'.' '.$dir.'\test_server\server_test.py 2>&1';
+        // exec($py_file, $output, $result);
 
+        // return redirect()->intended(RouteServiceProvider::HOME)
+        // ->with(['message' => $output[0],
+        // 'status' => 'info']);
         return redirect()->intended(RouteServiceProvider::HOME)
-        ->with(['message' => $output[0],
-        'status' => 'info']);
+        ->with(['message' => 'この機能は現在準備中です。',
+                'status' => 'info']);
     }
 }
