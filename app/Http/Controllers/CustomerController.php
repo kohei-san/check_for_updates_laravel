aa279427ff29d07d75f73599c3f8ec455997b296
kkenji-rmg <79568046+kkenji-rmg@users.noreply.github.com>
2021-10-01 16:52:38 +0900

    customer レイアウト修正

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Customer;
 use App\Models\CustomerPage;
 use App\Models\PageHtml;
+use App\Models\CreateHtml;
 use Illuminate\Http\Request;
 use Kyslik\ColumnSortable\Sortable;
 
@@ -39,10 +40,19 @@ class CustomerController extends Controller
     {
         $customer_id = $id;
         // $customer_pages = CustomerPage::where('customer_id', $customer_id)->get();
-        $customer_pages = CustomerPage::with(['customer', 'page_html'])
+        $customerPages = CustomerPage::with(['customer', 'page_html', 'short_diff'])
                             ->where('customer_id', $customer_id)
                             ->get();
 
-        return view('show-customer')->with('customer_pages', $customer_pages);
+        $htmlPreDirsModel = CreateHtml::orderBy("create_html_id", 'desc')->limit(3)->get();
+        foreach ($htmlPreDirsModel as $key => $htmlPreDirmodel){
+            $htmlPreDir[$key] = "Http\Controllers\\python\\acquired_data\\" . $htmlPreDirmodel["filename_timestamp"] ."\\html\\";
+        }
+
+        $htmlShortDifDir = __DIR__ . "\\python\\different\\short_term\\";
+
+        return view('show-customer')->with('customerPages', $customerPages)
+                                    ->with('htmlPreDir', $htmlPreDir)
+                                    ->with('htmlShortDifDir', $htmlShortDifDir);
     }
 }
--- resources/views/show-customer.blade.php
+++ resources/views/show-customer.blade.php
@@ -6,38 +6,142 @@
     </x-slot>
 
     <section class="text-gray-600 body-font">
-    <div class="xl:container px-5 py-24 mx-auto">
+    <div class="xl:container px-5 py-10 mx-auto">
+        <section class="text-gray-600 body-font">
+            <div class="container px-5 mb-5 mx-auto flex items-center md:flex-row flex-col">
+              <div class="flex flex-col md:pr-10 md:mb-0 mb-6 pr-0 w-full md:w-auto md:text-left text-center">
+                <h2 class="text-s text-blue-800 tracking-widest font-medium title-font mb-1">{{ $customerPages[0]->customer->support_id }}</h2>
+                <h1 class="md:text-3xl text-2xl font-medium font-mono text-gray-900">{{ $customerPages[0]->customer->customer_name }}</h1>
+              </div>
+              <div class="flex md:ml-auto md:mr-10 mx-auto items-center flex-shrink-0 space-x-4" id="show_customer">
+                @if($customerPages[0]->customer->line_register != null)
+                    @if($customerPages[0]->customer->line_register->line_flg == 1)
+                        <x-span :registered="true" class="" id="{{$customerPages[0]->customer->support_id}}">
+                            {{ __('登録済み') }}
+                        </x-span>
+                    @else
+                        <x-span :registered="false" class="" id="{{$customerPages[0]->customer->support_id}}">
+                            {{ __('未登録') }}
+                        </x-span>
+                    @endif
+                @else
+                    <x-span :registered="false" class="" id="{{$customerPages[0]->customer->support_id}}">
+                        {{ __('未登録') }}
+                    </x-span>
+                @endif
+              </div>
+            </div>
+        </section>
+                
         <div class="w-full mx-auto overflow-auto">
         <table class="table-auto border-separate border w-full text-left whitespace-no-wrap">
             <thead>
             <tr>
                 <x-table-th>{{ __('No.') }}</x-table-th>
-                <x-table-th>{{ __('サポートID') }}</x-table-th>
-                <x-table-th>{{ __('顧客名') }}</x-table-th>
-                <x-table-th>{{ __('担当者名') }}</x-table-th>
+                {{-- <x-table-th>{{ __('サポートID') }}</x-table-th>
+                <x-table-th>{{ __('顧客名') }}</x-table-th> 
+                --}}
                 <x-table-th>{{ __('URL') }}</x-table-th>
-                <x-table-th>{{ __('〇〇') }}</x-table-th>
-                <x-table-th>{{ __('〇〇') }}</x-table-th>
-                <x-table-th>{{ __('') }}</x-table-th>
+                <x-table-th>{{ __('前回ファイル') }}</x-table-th>
+                <x-table-th>{{ __('最新ファイル') }}</x-table-th>
+                <x-table-th>{{ __('差分あり') }}</x-table-th>
+                <x-table-th>{{ __('ファイル取得日') }}</x-table-th>
             </tr>
             </thead>
             <!-- 顧客情報表示 -->
             
-            @foreach($customer_pages as $customer_page)
+            @foreach($customerPages as $customerPage)
                 <tbody>
                     <tr>
                         <x-table-td :active="$loop->iteration % 2 == 1">{{ __($loop->iteration) }}</x-table-td>
-                        <x-table-td :active="$loop->iteration % 2 == 1">{{ __($customer_page->customer->support_id) }}</x-table-td>
-                        <x-table-td :active="$loop->iteration % 2 == 1">{{ __($customer_page->customer->customer_name) }}</x-table-td>
-                        <x-table-td :active="$loop->iteration % 2 == 1">{{ __('担当者名') }}</x-table-td>
+
                         <x-table-td :active="$loop->iteration % 2 == 1">
-                            <a href="{{ $customer_page->page_url }}">    
-                                {{ __($customer_page->page_url) }}
+                            <a href="{{ $customerPage->page_url }}">    
+                                {{ __($customerPage->page_url) }}
                             </a>
                         </x-table-td>
-                        <x-table-td :active="$loop->iteration % 2 == 1">{{ __('') }}</x-table-td>
-                        <x-table-td :active="$loop->iteration % 2 == 1">{{ __('') }}</x-table-td>
-                        <x-table-td :active="$loop->iteration % 2 == 1">{{ __('') }}</x-table-td>
+                        {{-- ▼前回、前々回の表示 --}}
+                        @php
+                            $newhtmlfile = $htmlPreDir[0] . $customerPage->page_id . "." . "html";
+                            $pre1htmlfile = $htmlPreDir[1] . $customerPage->page_id . "." . "html";
+                            $pre2htmlfile = $htmlPreDir[2] . $customerPage->page_id . "." . "html";
+                        @endphp
+                        @if( Illuminate\Support\Facades\File::exists(app_path($newhtmlfile)))
+
+                            @if( Illuminate\Support\Facades\File::exists($pre1htmlfile))
+                                <x-table-td :active="$loop->iteration % 2 == 1">
+                                    <a href="{{ Illuminate\Support\Facades\File::get($pre1htmlfile) }}"class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" >
+                                        {{ __("開く") }}
+                                    </a>
+                                </x-table-td>
+                            @else
+                                <x-table-td :active="$loop->iteration % 2 == 1">
+
+                                </x-table-td>
+                            @endif
+                            <x-table-td :active="$loop->iteration % 2 == 1">
+                                <a href="{{ app_path($newhtmlfile) }}"class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" >
+                                    {{ __("開く") }}
+                                </a>
+                            </x-table-td>
+                        @elseif( Illuminate\Support\Facades\File::exists(app_path($pre1htmlfile)))
+
+                            @if( Illuminate\Support\Facades\File::exists(app_path($pre2htmlfile)))
+                                <x-table-td :active="$loop->iteration % 2 == 1">
+                                    <a href="{{ app_path($pre2htmlfile) }}"class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" >
+                                        {{ __("開く") }}
+                                    </a>
+                                </x-table-td>
+                            @else
+                                <x-table-td :active="$loop->iteration % 2 == 1">
+
+                                </x-table-td>
+                            @endif
+                            <x-table-td :active="$loop->iteration % 2 == 1">
+                                <a href="{{ app_path($pre1htmlfile) }}"class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" >
+                                    {{ __("開く") }}
+                                </a>
+                            </x-table-td>
+                        @else
+
+                            <x-table-td :active="$loop->iteration % 2 == 1">
+
+                            </x-table-td>
+
+                            <x-table-td :active="$loop->iteration % 2 == 1">
+
+                            </x-table-td>
+                        
+                        @endif
+                        {{-- ▲ --}}
+
+                        {{-- ▼差分の表示 --}}
+                        @if($customerPage->short_diff->difference_flg == 1)
+                        <x-table-td :active="$loop->iteration % 2 == 1">
+                            @php
+                            $htmlfile = $htmlShortDifDir . $customerPage->page_id . "." . "html";
+                            @endphp
+                            @if( Illuminate\Support\Facades\File::exists($htmlfile))
+                            {{-- <x-sabun-a href="#" :haveDifference="true" onclick="window.open({{$htmlfile}}, 'chrome','width=1280,height=720,noopener'); return false;" class=""> --}}
+                            <x-sabun-a href="#" :haveDifference="true">
+                                {{ __('差分あり') }}
+                            </x-sabun-a>
+                            @else
+                            <x-sabun-a href="#" :haveDifference="true" class="">
+                                {{ __('差分あり') }}
+                            </x-sabun-a>
+                            @endif
+                        </x-table-td>
+                        @else
+                        <x-table-td :active="$loop->iteration % 2 == 1">
+                            <x-sabun-a :haveDifference="false" class="">
+                            {{ __('差分なし') }}
+                            </x-sabun-a>
+                        </x-table-td>
+                        @endif
+                        {{-- ▲ --}}
+
+                        <x-table-td :active="$loop->iteration % 2 == 1">{{ __($customerPage->page_html->time_stamp_htmlsrc) }}</x-table-td>
                     </tr>
                 </tbody>
             @endforeach
