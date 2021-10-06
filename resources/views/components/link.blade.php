e53daa8fa1ea0e603502764f67ce26fa595d094f
rm-miyatake <89385120+rm-miyatake@users.noreply.github.com>
2021-10-06 11:02:45 +0900

    リンクを別タブで開くように変更

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -4,6 +4,6 @@
     $classes = "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded";
 @endphp
 
-<a href="{{ $url }}" {{ $attributes->merge(['class' => $classes]) }}>
+<a href="{{ $url }}" target="_blank" rel="noopener" {{ $attributes->merge(['class' => $classes]) }}>
     {{ $slot }}
 </a>
