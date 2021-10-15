2b618cc3dc1efd76aa5072be1cea39e623f6410e
rm-miyatake <89385120+rm-miyatake@users.noreply.github.com>
2021-10-15 15:45:33 +0900

    vue.js導入、失敗

--- resources/views/analysis.blade.php
+++ resources/views/analysis.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+{{-- <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('corsin') }}
@@ -13,57 +13,18 @@
     {{$activeCallCount}}
     {{$reviewCount}}
 
-    <template>
-      <div class="small">
-        <line-chart :chart-data="datacollection"></line-chart>
-        <button @click="fillData()">Randomize</button>
-      </div>
-    </template>
-    
-    <script>
-      import LineChart from './LineChart.js'
-    
-      export default {
-        components: {
-          LineChart
-        },
-        data () {
-          return {
-            datacollection: null
-          }
-        },
-        mounted () {
-          this.fillData()
-        },
-        methods: {
-          fillData () {
-            this.datacollection = {
-              labels: [this.getRandomInt(), this.getRandomInt()],
-              datasets: [
-                {
-                  label: 'Data One',
-                  backgroundColor: '#f87979',
-                  data: [this.getRandomInt(), this.getRandomInt()]
-                }, {
-                  label: 'Data One',
-                  backgroundColor: '#f87979',
-                  data: [this.getRandomInt(), this.getRandomInt()]
-                }
-              ]
-            }
-          },
-          getRandomInt () {
-            return Math.floor(Math.random() * (50 - 5 + 1)) + 5
-          }
-        }
-      }
-    </script>
-    
-    <style>
-      .small {
-        max-width: 600px;
-        margin:  150px auto;
-      }
-    </style>
+
+</x-app-layout> --}}
+
+<x-app-layout>
+
+<x-slot name="header">
+  <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      {{ __('corsin') }}
+  </h2>
+</x-slot>
+
+    <example-component></example-component>
+
     
 </x-app-layout>
